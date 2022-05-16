204221c27a65e207c11093fd4c06dcb863d7c08e
Yasser <yasserbal@icloud.com>
2022-05-16 17:35:12 +0200

    Added logic to put cookies in shop

--- scripts/gameStats.js
+++ scripts/gameStats.js
@@ -1,7 +1,7 @@
 //Will contain the model and logic for the game-stats-panel
 //Cookies Baked,Cookies in Shop, Credits
 let totalCookiesBaked = 0;
-
+let cookiesInShop = 0;
 
 export function update() {
     console.log('@@GameStats Update')
@@ -12,8 +12,11 @@ export function draw() {
 }
 export function bakeCookie() {
     totalCookiesBaked++
+    cookiesInShop++
 }
 function drawCookiesBaked() {
     document.getElementById('cookies-baked').innerText = '';
     document.getElementById('cookies-baked').innerText = totalCookiesBaked;
+    document.getElementById('cookies-shop').innerText = '';
+    document.getElementById('cookies-shop').innerText = cookiesInShop;
 }
